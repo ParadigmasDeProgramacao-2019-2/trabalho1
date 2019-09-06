b4014870111de22c688cfafe4b508eb341f104db
DanMke <danmke@hotmail.com>
2019-09-06 11:19:06 -0300

    Corrected utils errors and update user attributes
    Co-authored-by: joberth.rogers18 <joberth.rogers18@gmail.com>
    Co-authored-by: gabiMSilva <gabriela.medeiros2010@hotmail.com.br>

--- Usuario.hs
+++ Usuario.hs
@@ -1,14 +1,11 @@
-module Usuario (QtdPecas, Nome, Jogador, cadastraJogador) where
+module Usuario (Nome, Jogador, cadastraJogador) where
 
 import Utils
 
-type QtdPecas = Int
 type Nome = String
-type Id = Int
-data Jogador = Jogador Nome QtdPecas deriving (Show)
+data Jogador = Jogador Nome deriving (Show)
 
 cadastraJogador :: IO Jogador
 cadastraJogador = do
-    nome<-getString("Digite o seu nome:\n")
-
-    return (Jogador nome 32)
+    nome <- getString("Digite o seu nome:\n")
+    return (Jogador nome)
