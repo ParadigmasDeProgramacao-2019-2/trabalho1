860eeec2d4f70c9a1aacc7fea6d063e3c0034e24
DanMke <danmke@hotmail.com>
2019-09-06 10:57:46 -0300

    Added functions to take movement and change elements in movement
    Co-authored-by: gabiMSilva <gabriela.medeiros2010@hotmail.com.br>

--- Movimento.hs
+++ Movimento.hs
@@ -1,49 +1,49 @@
-module Movimento (pegaLinha, pegaColuna, pegaDirecao, mapeiaLetraColuna, defineDestinoMatriz) where
+module Movimento (pegaLinha, pegaColuna, pegaDirecao, mapeiaLetraLinha, defineDestinoMatriz) where
 
 import Data.Char
 import Validacoes
-
-{--
-    Função para capturar o input da linha
---}
-pegaLinha :: IO Int
-pegaLinha = do
-    putStrLn "Digite a linha da peca que vai se movimentar [1..7]:"
-    linhaInput <- getLine
-    if (validaEntradaLinhaColuna (read linhaInput) == False) 
-        then do
-            putStrLn "Linha invalida [1..7]" 
-            pegaLinha
-        else do
-            return (read linhaInput)
-
-{--
-    Mapeamento das letras das colunas com numeros da matriz
---}
-mapeiaLetraColuna :: Char -> Int
-mapeiaLetraColuna 'A' = 1
-mapeiaLetraColuna 'B' = 2
-mapeiaLetraColuna 'C' = 3
-mapeiaLetraColuna 'D' = 4
-mapeiaLetraColuna 'E' = 5
-mapeiaLetraColuna 'F' = 6
-mapeiaLetraColuna 'G' = 7
-mapeiaLetraColuna  _  = 8
 
 {--
     Função para capturar o input da coluna
 --}
 pegaColuna :: IO Int
 pegaColuna = do
-    putStrLn "Digite a coluna da peca que vai se movimentar [A..G]:"
-    colunaInput <- getChar
-    getChar
-    if (validaEntradaLinhaColuna (mapeiaLetraColuna (toUpper colunaInput)) == False) 
+    putStrLn "Digite a coluna da peca que vai se movimentar [1..7]:"
+    colunaInput <- getLine
+    if (validaEntradaLinhaColuna (read colunaInput) == False) 
         then do
-            putStrLn "Coluna invalida [A..G]" 
+            putStrLn "Coluna invalida [1..7]" 
             pegaColuna
         else do
-            return (mapeiaLetraColuna (toUpper colunaInput))
+            return (read colunaInput)
+
+{--
+    Mapeamento das letras das colunas com numeros da matriz
+--}
+mapeiaLetraLinha :: Char -> Int
+mapeiaLetraLinha 'A' = 1
+mapeiaLetraLinha 'B' = 2
+mapeiaLetraLinha 'C' = 3
+mapeiaLetraLinha 'D' = 4
+mapeiaLetraLinha 'E' = 5
+mapeiaLetraLinha 'F' = 6
+mapeiaLetraLinha 'G' = 7
+mapeiaLetraLinha  _  = 8
+
+{--
+    Função para capturar o input da linha
+--}
+pegaLinha :: IO Int
+pegaLinha = do
+    putStrLn "Digite a linha da peca que vai se movimentar [A..G]:"
+    linhaInput <- getChar
+    getChar
+    if (validaEntradaLinhaColuna (mapeiaLetraLinha (toUpper linhaInput)) == False) 
+        then do
+            putStrLn "Linha invalida [A..G]" 
+            pegaLinha
+        else do
+            return (mapeiaLetraLinha (toUpper linhaInput))
 
 {--
     Função para capturar o input da direcao
@@ -62,8 +62,27 @@ pegaDirecao = do
 {--
     Função para definir para onde a peça será movida
 --}
-defineDestinoMatriz :: Int -> Int -> Int -> (Int,Int)
-defineDestinoMatriz linha coluna 1 = (linha - 1, coluna) 
-defineDestinoMatriz linha coluna 2 = (linha + 1, coluna) 
-defineDestinoMatriz linha coluna 3 = (linha, coluna - 1) 
-defineDestinoMatriz linha coluna 4 = (linha, coluna + 1) 
+defineDestinoMatriz :: Int -> Int -> Int -> Int -> (Int,Int)
+defineDestinoMatriz linha coluna 1 incremento = (linha - incremento, coluna) 
+defineDestinoMatriz linha coluna 2 incremento = (linha + incremento, coluna) 
+defineDestinoMatriz linha coluna 3 incremento = (linha, coluna - incremento) 
+defineDestinoMatriz linha coluna 4 incremento = (linha, coluna + incremento) 
+
+{--
+    Função para pegar os movimentos e realizar a jogada
+--}
+
+pegaMovimento :: Matrix Char -> IO()
+pegaMovimento tabuleiro = do
+    printarTabuleiro tabuleiro
+    linha <- pegaLinha
+    coluna <- pegaColuna
+    direcao <- pegaDirecao
+    fazJogada linha coluna direcao tabuleiro
+
+{--
+    Função para realizar a jogada, alterar os 3 elementos que sao alterados ao fazer uma jogada
+--}    
+fazJogada :: Int -> Int -> Int -> Matrix Char -> IO()
+fazJogada linha coluna direcao tabuleiro = do
+    pegaMovimento (setElem '-' (linha, coluna) (setElem '-' (defineDestinoMatriz linha coluna direcao 1) (setElem 'O' (defineDestinoMatriz linha coluna direcao 2) tabuleiro)))
--- main.hs
+++ main.hs
@@ -8,15 +8,3 @@ import Data.Matrix
 main :: IO()
 main = do
     menu
-
-pegaMovimento = do
-    linha <- pegaLinha
-    coluna <- pegaColuna
-    direcao <- pegaDirecao
-    print (linha)
-    print (coluna)
-    print (direcao)
-    -- verificar a jogada
-    -- se for valida ele troca os 3 elementos
-    -- setElem '-' (defineDestinoMatriz linha coluna direcao) tabuleiro
-    -- printarTabuleiro tabuleiro
